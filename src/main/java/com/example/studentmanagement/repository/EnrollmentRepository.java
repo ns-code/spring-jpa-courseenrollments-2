e16dbc6f46257f196049d22355a5a427a343c020
Nalla Senthilnathan <5382324+mapteb@users.noreply.github.com>
2026-01-11 13:57:36 -0500

    cleanup

--- src/main/java/com/example/studentmanagement/repository/EnrollmentRepository.java
+++ src/main/java/com/example/studentmanagement/repository/EnrollmentRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
-@Repository
 public interface EnrollmentRepository extends JpaRepository<Enrollment, EnrollmentId> {
     List<Enrollment> findById_StudentId(Integer studentId);
     List<Enrollment> findById_CourseId(Integer courseId);
